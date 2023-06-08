00f648c8a170b6dd1d6da835f8634ee55ffb774a
SongDongHee <mushroom041215@gmail.com>
2023-06-08 18:29:03 +0900

    refer to pixegami / nextjs-blog-tutorial

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
+import PostPreview from "@/components/PostPreview";
+import getPostMetadata from "@/components/getPostMetadata";
+import Link from "next/link";
+
 const HomePage = () => {
-    return <h1>Hello From NextJS</h1>;
-}
+    const PostMetadata = getPostMetadata();
+    const postPreviews = PostMetadata.map((post) => (
+        <PostPreview key = {post.slug} {...post} />
+    ));
+    return <div className="grid grid-cols-1 md:grid-cols-2 gap-4">{postPreviews}</div>
+};
 
 export default HomePage;
